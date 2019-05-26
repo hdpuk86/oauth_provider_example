441f63d158f2ad72855ccfe7145137446f706c0c
hdpuk86 <hayley.d.prior@gmail.com>
2019-05-26 14:20:04 +0100

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static#index'
+
   devise_for :users
   use_doorkeeper do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
